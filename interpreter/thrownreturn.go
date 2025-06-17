7fbe87a5f7c2475b11203e16863aacc8331a84c0
DY Dev <dydev420@gmail.com>
2025-06-17 13:06:17 +0530

    remove fmt from ThrownReturn because it returns a simple string

--- interpreter/thrownreturn.go
+++ interpreter/thrownreturn.go
@@ -1,13 +1,9 @@
 package interpreter
 
-import (
-	"fmt"
-)
-
 type ThrownReturn struct {
 	value any
 }
 
 func (re *ThrownReturn) Error() string {
-	return fmt.Sprintf("Not an error. If this shows up as error in logs, something is wrong with handling of return value")
+	return "Not an error. If this shows up as error in logs, something is wrong with handling of return value"
 }
